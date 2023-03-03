b917cd972bc2712cf714c11c0891485a64c5896a
Nikolay Kalashnikov <n.kalashnikov@crpo.su>
2023-03-03 16:07:15 +0100

    delete instance variables from partials

--- app/controllers/web/posts/comments_controller.rb
+++ app/controllers/web/posts/comments_controller.rb
@@ -11,7 +11,7 @@ class Web::Posts::CommentsController < Web::ApplicationController
     else
       @comments = @post.comments.arrange(order: { created_at: :desc })
       @like = PostLike.find_by(post_id: @post.id, user_id: current_user.id)
-      @pure_comment = @post.comments.build
+      @empty_comment = @post.comments.build
       flash.now[:failure] = t('.failure')
       render 'web/posts/show', status: :unprocessable_entity
     end
--- app/controllers/web/posts_controller.rb
+++ app/controllers/web/posts_controller.rb
@@ -10,7 +10,7 @@ class Web::PostsController < Web::ApplicationController
     @post = Post.find(params[:id])
     @comments = @post.comments.arrange(order: { created_at: :desc })
     @comment = @post.comments.build
-    @pure_comment = @post.comments.build
+    @empty_comment = @post.comments.build
     @like = PostLike.find_by(post_id: @post.id, user_id: current_user&.id)
   end
 
