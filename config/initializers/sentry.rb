df6612a888f0fbee1b1c4e52590aba11a3f36282
Nikolay Kalashnikov <n.kalashnikov@crpo.su>
2023-03-19 17:07:36 +0100

    add dotenv-rails to project

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Sentry.init do |config|
-  config.dsn = 'https://8b83ba4c4a6047339955656c63db5986@o4504640144080896.ingest.sentry.io/4504640188645376'
+  config.dsn = ENV.fetch('SENTRY_CONFIG_DSN', nil)
   config.breadcrumbs_logger = %i[active_support_logger http_logger]
 
   # Set traces_sample_rate to 1.0 to capture 100%
